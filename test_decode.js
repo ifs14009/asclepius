827dad8b240cf5ac5e4d8d1742b50b2296e5371d
Ranty Deviana Siahaan <rantysiahaan01@gmail.com>
2026-07-21 08:39:22 +0700

    Delete test_decode.js

--- test_decode.js
+++
@@ -1,9 +0,0 @@
-const tf = require('@tensorflow/tfjs-node');
-
-try {
-  const buffer = Buffer.from('hello world this is a text file');
-  const tensor = tf.node.decodeImage(buffer, 3);
-  console.log('SUCCESS, tensor shape:', tensor.shape);
-} catch (error) {
-  console.log('ERROR THROWN:', error.message);
-}
